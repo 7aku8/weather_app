6de888c3b576188c65f9e5eb1d5739926f5ac106
Jakub Wolak <jakubwolak.123@gmail.com>
2020-04-18 15:05:08 +0200

    some css added

--- weather/views.py
+++ weather/views.py
@@ -7,7 +7,7 @@ import urllib.request as rq
 
 
 def index(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.POST['city']:
         city = request.POST['city']
 
         source = rq.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
@@ -16,15 +16,19 @@ def index(request):
         list_of_data = json.loads(source)
 
         data = {
+            'results': True,
+            'city': city,
             'country_code': str(list_of_data['sys']['country']),
             'coordinate': str(list_of_data['coord']['lon']) + ',  ' + str(list_of_data['coord']['lat']),
-            'temp': str(list_of_data['main']['temp']),
+            'temp': str(list_of_data['main']['temp'] - 273),
             'pressure': str(list_of_data['main']['pressure']),
             'humidity': str(list_of_data['main']['humidity']),
         }
         print(data)
 
     else:
-        data = {}
+        data = {
+            'results': False,
+        }
 
     return render(request, 'weather/index.html', data)
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # weather api key
 WEATHER_API_KEY = 'ca5612aa1e82c360e2f47c24414423b9'
