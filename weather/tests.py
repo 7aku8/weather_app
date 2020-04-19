4fb775f0675a8cecd7b8207df4af5f5ebdaab6c3
Jakub Wolak <jakubwolak.123@gmail.com>
2020-04-19 21:15:13 +0200

    add some tests

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, Client
+from django.urls import reverse
+from urllib.error import HTTPError
 
-# Create your tests here.
+
+class ViewTestCase(SimpleTestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('weather:index')
+        self.valid_city = {
+            'city': 'Rzeszow',
+        }
+        self.invalid_city = {
+            'city': 'Reszdgwsw',
+        }
+
+    def test_index_response_without_data(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_response_vaild_city(self):
+        response = self.client.post(self.url, self.valid_city)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_response_invalid_city(self):
+        response = self.client.post(self.url, self.invalid_city)
+
+        self.assertRaises(HTTPError, response)
